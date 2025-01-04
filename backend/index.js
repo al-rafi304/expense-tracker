00f2d9f6d4d70d7a6db4903697ca60d61ee32adb
Mohammad Al Rafi <arafi304@gmail.com>
2025-01-04 23:40:41 +0600

    Cors Dev environment

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'express-async-errors'
 import * as dotenv from "dotenv";
+import cors from 'cors';
 
 dotenv.config();
 
@@ -12,7 +13,12 @@ import expenseRoutes from './routes/expense.js'
 const server = express()
 const port = process.env.PORT || 3000
 
-
+server.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from the Vite dev server
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+    credentials: true, // If you need to send cookies or authorization headers
+    exposedHeaders: 'Authorization',
+  }));
 server.use(express.json())
 server.use('/api/v1/auth', authRoutes),
 server.use('/api/v1/user', userRoutes)
